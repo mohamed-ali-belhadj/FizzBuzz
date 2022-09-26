740a8d1244ef0a5fb2b411c1766cf48c40ed458d
Mohamed Ali BELHADJ <medalibelhadj2@gmail.com>
2022-09-26 10:32:49 +0100

    Create Project

---
+++ fizzBuzz/fizzBuzz/fizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  fizzBuzzApp.swift
+//  fizzBuzz
+//
+//  Created by Zied Gmar on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct fizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
