358f272ee77cc260eee7425789e9c34c064727a7
Mohamed Ali BELHADJ <medalibelhadj2@gmail.com>
2022-09-26 11:31:30 +0100

    Create Unit Test

--- fizzBuzz/fizzBuzzTests/fizzBuzzTests.swift
+++ fizzBuzz/fizzBuzzTests/fizzBuzzTests.swift
@@ -9,28 +9,35 @@ import XCTest
 @testable import fizzBuzz
 
 class fizzBuzzTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var interactor: Interactor? = nil
+    
+    
+    override func tearDown() {
+        interactor = nil
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testUtils() {
+        let hugeRange = -1_000_000...1_000_000
+        let result = Int.random(in: hugeRange).divisible(by: Int.random(in: hugeRange))
+        
+        XCTAssertEqual(10.divisible(by: 2), true)
+        
+        XCTAssertEqual(0.divisible(by: 0), false)
+        XCTAssertEqual(Int.random(in: hugeRange).divisible(by: 0), false)
+        
+        XCTAssertNotEqual(result, nil)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testBasicOutputUsingRandomIntegers() async {
+        interactor = .init(limit: Int.random(in: 100...1_000), firstNumber: Int.random(in: 1...10), secondNumber: Int.random(in: 1...10), firstWord: "Fizz", secondWord: "Buzz")
+        let result = await interactor?.invoke()
+        
+        XCTAssertEqual(result?.count, interactor?.limit)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testInputEqualsOne() async {
+        interactor = .init(limit: 1, firstNumber: 1, secondNumber: 1, firstWord: "Fizz", secondWord: "Buzz")
+        let result = await interactor?.invoke()
+        XCTAssertEqual(["FizzBuzz"], result)
     }
-
 }
